e8285ac448cc00fcdd800673d0d12dc6b284afb3
Austin Ramos <Austinramos@wustl.edu>
2016-11-04 23:01:55 -0500

    User

---
+++ alertUs/alertUs/User.swift
@@ -0,0 +1,27 @@
+//
+//  User.swift
+//  alertUs
+//
+//  Created by Austin Ramo on 11/4/16.
+//  Copyright © 2016 ARIA. All rights reserved.
+//
+
+import Foundation
+
+class User{
+    
+    
+    var name:String
+    var age : Int
+    var weight : Double
+    var height : Double
+    
+    init(name: String, age: Int, weight: Double, height: Double ){
+        self.age = age
+        self.name = name
+        self.weight = weight
+        self.height=height
+    }
+    
+    
+}
